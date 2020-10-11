f1ed106f686a1b91e238437fcee9c9ef73026b50
KevinDyke <kdyke058@gmail.com>
2020-10-11 23:54:29 +0100

    Update to UI using dynamic dispatch.

--- renderarea.cpp
+++ renderarea.cpp
@@ -1,12 +1,14 @@
 #include "renderarea.h"
 #include <QPaintEvent>
 #include <QPainter>
+#include <QtMath>
 
 RenderArea::RenderArea(QWidget *parent) : QWidget(parent),
     mBackgroundColor(QColor(0 ,0 ,255)),
     mShapeColor(255, 255, 255),
     mShape(Astroid)
 {
+    on_shape_changed();
 }
 
 QSize RenderArea::minimumSizeHint() const
@@ -19,36 +21,113 @@ QSize RenderArea::sizeHint() const
     return QSize(100,100);
 }
 
-void RenderArea::paintEvent(QPaintEvent *event)
+QPointF RenderArea::compute_astroid(float t)
 {
-    QPainter painter(this);
-    painter.setRenderHint(QPainter::Antialiasing,true);
+    float cos_t = cos(t);
+    float sin_t = sin(t);
 
+    float x = 2 * cos_t * cos_t * cos_t; // pow(cos_t,3)
+    float y = 2 * sin_t * sin_t * sin_t; // pow(sin_t,3)
+    return QPointF(x,y);
+}
+
+void RenderArea::on_shape_changed()
+{
     switch(mShape) {
         case Astroid:
-            mBackgroundColor = Qt::red;
+            m_Scale = 40;
+            m_IntervalLength = 2 * M_PI;
+            m_StepCount = 256;
             break;
 
         case Cycloid:
-            mBackgroundColor = Qt::green;
             break;
 
         case HuygensCycloid:
-            mBackgroundColor = Qt::blue;
             break;
 
         case HypoCycloid:
-            mBackgroundColor = Qt::yellow;
+            break;
+
+        case FutureCurve:
             break;
 
         default:
             break;
     }
+}
+
+QPointF RenderArea::compute(float t)
+{
+    switch(mShape) {
+        case Astroid:
+            return compute_astroid(t);
+            break;
+
+        case Cycloid:
+            return compute_cycloid(t);
+            break;
+
+        case HuygensCycloid:
+            return compute_huygens(t);
+            break;
+
+        case HypoCycloid:
+            return compute_hypo(t);
+            break;
+
+        case FutureCurve:
+            return compute_future_curve(t);
+            break;
+
+        default:
+            break;
+    }
+    return QPointF(0,0);
+}
+
+QPointF RenderArea::compute_cycloid(float t)
+{
+
+}
+
+QPointF RenderArea::compute_huygens(float t)
+{
+
+}
+
+QPointF RenderArea::compute_hypo(float t)
+{
+
+}
+
+QPointF RenderArea::compute_future_curve(float t)
+{
+
+}
+
+void RenderArea::paintEvent(QPaintEvent *event)
+{
+    QPainter painter(this);
+    painter.setRenderHint(QPainter::Antialiasing,true);
 
     painter.setBrush(mBackgroundColor);
     painter.setPen(mShapeColor);
 
     // drawing area
     painter.drawRect(this->rect());
-    painter.drawLine(this->rect().topLeft(), this->rect().bottomRight());
+
+    QPoint center = this->rect().center();
+    m_StepCount = 256;
+    m_Scale = 40;
+    float step = m_IntervalLength / m_StepCount;
+    for(float t = 0; t < m_IntervalLength; t += step){
+        QPointF point = compute(t);
+        QPoint pixel;
+        pixel.setX(point.x() * m_Scale + center.x());
+        pixel.setY(point.y() * m_Scale + center.y());
+
+        painter.drawPoint(pixel);
+    }
 }
+
--- renderarea.h
+++ renderarea.h
@@ -13,21 +13,34 @@ public:
     QSize minimumSizeHint() const Q_DECL_OVERRIDE;
     QSize sizeHint() const Q_DECL_OVERRIDE;
 
-    enum ShapeType { Astroid, Cycloid, HuygensCycloid, HypoCycloid };
+    enum ShapeType { Astroid, Cycloid, HuygensCycloid, HypoCycloid, FutureCurve };
 
     void setBackgroundColor(QColor color) { mBackgroundColor = color;}
     QColor backgroundColor() const {return mBackgroundColor;}
 
-    void setShape(ShapeType shape) { mShape = shape;}
+    void setShape(ShapeType shape) { mShape = shape; on_shape_changed(); }
     ShapeType shape() const { return mShape; }
 
 protected:
     void paintEvent(QPaintEvent *event) override;
 
 private:
+    void on_shape_changed();
     QColor mBackgroundColor;
     QColor mShapeColor;
     ShapeType mShape;
+
+    float m_IntervalLength;
+    float m_Scale;
+    int m_StepCount;
+
+    QPointF compute(float t); // dispatch function based on shape type
+    QPointF compute_astroid(float t);
+    QPointF compute_cycloid(float t);
+    QPointF compute_huygens(float t);
+    QPointF compute_hypo(float t);
+    QPointF compute_future_curve(float t);
+
 signals:
 
 };
